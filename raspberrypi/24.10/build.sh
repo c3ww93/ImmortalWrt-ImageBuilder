9dec4fe348f2887d7f7af6b217e6230709479fc2
yxs123321 <53416197+yxs123321@users.noreply.github.com>
2026-08-21 18:13:18 +0800

    Update build.sh

--- raspberrypi/24.10/build.sh
+++ raspberrypi/24.10/build.sh
@@ -76,6 +76,7 @@ PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
+PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 # ======== shell/custom-packages.sh =======
 # 合并imm仓库以外的第三方插件
 PACKAGES="$PACKAGES $CUSTOM_PACKAGES"
